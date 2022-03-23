23461bdec6e6ebf0015fa561826c81c3bbfe8460
Alexander Savchuk <123tachka123@gmail.com>
2022-03-23 16:43:43 +0200

    goToDashBoard component

---
+++ src/GoToDashBoard/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import s from "./s.module.css";
+
+const GoToDashBoard = () => {
+  return <button className={s.container}>Go to dashboard {"-->"}</button>;
+};
+
+export default GoToDashBoard;
